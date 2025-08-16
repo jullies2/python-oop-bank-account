68fa87370fe8b609c5512c8a293a54ffc5423f55
Iuliia <jullies045@gmail.com>
2025-08-16 22:24:52 +0300

    Добавлен класс Transaction, исправлены классы банковских счетов, добавлен промежуточный класс Account.

--- base_classes.py
+++ base_classes.py
@@ -1,76 +1,113 @@
 from abc import ABC, abstractmethod
-import json
+from transaction_class import Transaction
+from functools import wraps
+import datetime
+import json  
 
-class Account(ABC):
-    def __init__(self, balance: float = 0.):
-        self._balance = balance
-
+class AccountABC(ABC):
+             
     @abstractmethod
-    def make_deposit(self, amount: float) -> None:
-        """ Пополнение счета """
+    def make_deposit(self):
         pass
     
     @abstractmethod
-    def withdraw_money(self, amount: float) -> None:
-        """ Снятие средств со счета """
+    def withdraw_money(self):
         pass
 
     @abstractmethod
+    def get_balance(self):
+        pass
+
+class Account(AccountABC):
+    def __init__(self, balance: float = 0.):
+        self._balance: float = balance
+        self._transactions_list: list[Transaction] = []
+
+
+    def log_transaction(transaction_type: str):
+        def decorator(func):
+            @wraps(func)
+            def wrapper(self, *args, **kwargs):
+                transaction_amount = kwargs.get('amount')
+                if transaction_amount is None:
+                    param_names = func.__code__.co_varnames[1:]
+                    for name, arg in zip(param_names, args):
+                        if name == 'amount':
+                            transaction_amount = arg
+                            break
+                if transaction_amount is None:
+                    raise ValueError('Не удалось найти параметр amount.')
+                res = func(self, *args, **kwargs)
+                self._transactions_list.append(
+                    Transaction(
+                        type = transaction_type, 
+                        amount=transaction_amount,
+                        ))
+                return res
+            return wrapper
+        return decorator
+             
+    @log_transaction(transaction_type='DEPOSIT')
+    def make_deposit(self, amount):
+        self._balance += amount
+        self.log(f'Пополнение {amount}. Текущий баланс: {self._balance}')
+
     def get_balance(self) -> float:
         return self._balance
     
+    def get_all_transactions(self) -> None:
+        for transaction in self._transactions_list:
+            print(transaction)
+    
 class LoggingMixin():
-    def log(self, message: str) -> str:
+    def log(self, message: str) -> None:
         print(f'[Log] {message}')
 
 class SerializableMixin():
     """ Сохранение и загрузка данных в JSON """
     def to_json(self) -> str:
-        return json.dumps(self.__dict__)
+        def convert_obj(obj):
+            if hasattr(obj, 'to_dict'):
+                return obj.to_dict()
+            elif isinstance(obj, datetime.datetime):
+                return obj.isoformat()
+            return str(obj)
+
+        data = {k: convert_obj(v) for k, v in self.__dict__.items()}
+        
+        return json.dumps(data, indent=2)
 
 class SavingsAccount(Account, LoggingMixin, SerializableMixin):
-    def make_deposit(self, amount):
-        self._balance += amount
-        self.log(f'Пополнение {amount}. Текущий баланс: {self._balance}')
-
+    @Account.log_transaction(transaction_type='WITHDRAW')
     def withdraw_money(self, amount):
         if amount > self._balance:
             raise ValueError("Ошибка, недостаточно средств")
         self._balance -= amount
         self.log(f'Снятие {amount}. Текущий баланс: {self._balance}')
-
-    def get_balance(self):
-        return super().get_balance()
     
     def add_percent(self, percent: float = 5.):
         self._balance *= 1 + percent / 100
         self.log(f'Добавлен процент на остаток. Текущий баланс: {self._balance}')
 
 class CreditAccount(Account, LoggingMixin, SerializableMixin):
-
     def __init__(self, balance: float = 0. , limit = -10000):
         self._limit = limit
         super().__init__(balance)
 
-    def make_deposit(self, amount):
-        self._balance += amount
-        self.log(f'Пополнение {amount}. Текущий баланс: {self._balance}')
-
+    @Account.log_transaction(transaction_type='WITHDRAW')
     def withdraw_money(self, amount):
+        new_balance = self._balance - amount
+        if new_balance < self._limit:
+            fee = amount * 0.1
+            amount += fee
         self._balance -= amount
-        if self._limit > self._balance:
-            self._balance -= amount * 0.1
         self.log(f'Снятие {amount}. Текущий баланс: {self._balance}')
-
-    def get_balance(self):
-        return super().get_balance()
-    
+        
 if __name__ == '__main__':
 
-    savings = CreditAccount(1000.0)
+    savings = SavingsAccount(1000.0)
     savings.make_deposit(500.0)  # Баланс: 1500.0
     savings.withdraw_money(200.0)  # Баланс: 1300.0
-    savings.withdraw_money(100000.0)
-    json_data = savings.to_json()
-    print(json_data) 
-
+    savings.withdraw_money(100.0)
+    savings.add_percent()
+    savings.get_all_transactions()
